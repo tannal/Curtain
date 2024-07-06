1fbc473594e8ec708b3174d8904e92241acd6b2b
Barrier <zeqide@163.com>
2024-07-06 14:59:50 +0800

    1.21 port

--- src/main/java/dev/dubhe/curtain/mixins/rules/stackable_shulker_boxes/ItemStackStackableShulkerBoxesMixin.java
+++ src/main/java/dev/dubhe/curtain/mixins/rules/stackable_shulker_boxes/ItemStackStackableShulkerBoxesMixin.java
@@ -1,26 +1,22 @@
-package dev.dubhe.curtain.mixins.rules.empty_shulker_box_stack_always;
+package dev.dubhe.curtain.mixins.rules.stackable_shulker_boxes;
+
 
 import dev.dubhe.curtain.CurtainRules;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemContainerContents;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
-public abstract class ItemStackMixin {
-    @Shadow
-    public abstract Item getItem();
-
+public class ItemStackStackableShulkerBoxesMixin {
     @Inject(method = "getMaxStackSize", at = @At("HEAD"), cancellable = true)
     private void getCMMAxStackSize(CallbackInfoReturnable<Integer> cir) {
-        if (CurtainRules.emptyShulkerBoxStackAlways
+        if (CurtainRules.shulkerBoxStackSize > 1
                 && ((ItemStack) ((Object) this)).getItem() instanceof BlockItem blockItem
                 && blockItem.getBlock() instanceof ShulkerBoxBlock
                 && ((ItemStack) ((Object) this)).getOrDefault(DataComponents.CONTAINER, ItemContainerContents.EMPTY).stream().findAny().isEmpty()
